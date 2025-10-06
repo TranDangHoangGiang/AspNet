7c9ce37cda3b0c9b203a316517f7e26310c20a9e
Hoang Giang <21011125@st.phenikaa-uni.edu.vn>
2025-10-06 13:54:35 +0700

    Commit lan 3

--- AspNet/Program.cs
+++ AspNet/Program.cs
@@ -10,7 +10,7 @@ namespace AspNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World Ahihi");
         }
     }
 }
